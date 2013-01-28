bb3fb52b150a62bcd8f663e06f5aa31a1933efbf
ichevdar <ichevdar@skbkontur.ru>
2013-01-28 07:34:05 +0000

    Switch stack mutator added

--- GrEmit/GrEmit/StackMutators/SwitchStackMutator.cs
+++ GrEmit/GrEmit/StackMutators/SwitchStackMutator.cs
@@ -1,14 +1,19 @@
 using System;
 using System.Collections.Generic;
 
+using GrEmit.InstructionParameters;
+
 namespace GrEmit.StackMutators
 {
     internal class SwitchStackMutator : StackMutator
     {
         public override void Mutate(GroboIL il, ILInstructionParameter parameter, ref Stack<Type> stack)
         {
+            var labels = ((LabelsILInstructionParameter)parameter).Labels;
             CheckNotEmpty(il, stack);
             CheckNotStruct(il, stack.Pop());
+            foreach(var label in labels)
+                SaveOrCheck(il, stack, label);
         }
     }
 }
