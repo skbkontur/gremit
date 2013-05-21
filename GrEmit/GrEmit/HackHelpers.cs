9fad0be94775fc4a87aa263ad18df9977dcbadc6
dgorlov <dgorlov@skbkontur.ru>
2013-05-21 11:45:31 +0000

    HackHelpers + tests

---
+++ GrEmit/GrEmit/HackHelpers.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace GrEmit
+{
+    public static class HackHelpers
+    {
+        public static T CreateDelegate<T>(DynamicMethod dm, object target) where T : class
+        {
+            Delegate @delegate = dm.CreateDelegate(typeof(T), target);
+            var result = (@delegate as T);
+            if(result == null)
+                throw new ArgumentException(String.Format("Type {0} not a delegate", typeof(T)));
+            return result;
+        }
+
+        public static Type GetValueTypeForNullableOrNull(Type mayBeNullable)
+        {
+            if(!mayBeNullable.IsGenericType || mayBeNullable.IsGenericTypeDefinition ||
+               mayBeNullable.GetGenericTypeDefinition() != typeof(Nullable<>)) return null;
+            Type valueType = mayBeNullable.GetGenericArguments()[0];
+            return valueType;
+        }
+
+        public static T CreateDelegate<T>(DynamicMethod dm) where T : class
+        {
+            Delegate @delegate = dm.CreateDelegate(typeof(T));
+            var result = (@delegate as T);
+            if(result == null)
+                throw new ArgumentException(String.Format("Type {0} not a delegate", typeof(T)));
+            return result;
+        }
+
+        public static ConstructorInfo GetObjectConstruction<T>(Expression<Func<T>> constructorCall,
+                                                               params Type[] classGenericArgs)
+        {
+            Expression expression = constructorCall.Body;
+            ConstructorInfo sourceCi;
+            switch(expression.NodeType)
+            {
+            case ExpressionType.Convert:
+                sourceCi = ObjectConstructionFromConvert(expression);
+                break;
+            case ExpressionType.New:
+                sourceCi = ObjectConstruction(expression);
+                break;
+            default:
+                throw new NotSupportedException("Bad expression " + constructorCall);
+            }
+            if(typeof(T).IsValueType && sourceCi == null)
+                throw new NotSupportedException("Struct creation without arguments");
+            Type type = typeof(T);
+            Type resultReflectedType = type.IsGenericType
+                                           ? type.GetGenericTypeDefinition().MakeGenericType(classGenericArgs)
+                                           : type;
+            MethodBase methodBase = MethodBase.GetMethodFromHandle(
+                sourceCi.MethodHandle, resultReflectedType.TypeHandle);
+            var constructedCtorForResultType = (ConstructorInfo)methodBase;
+            return constructedCtorForResultType;
+        }
+
+        public static MethodInfo ConstructMethodDefinition<T>(Expression<Action<T>> callExpr, Type[] methodGenericArgs)
+        {
+            var methodCallExpression = (MethodCallExpression)callExpr.Body;
+            MethodInfo methodInfo = methodCallExpression.Method;
+            if(!methodInfo.IsGenericMethod)
+                return methodInfo;
+            return methodInfo.GetGenericMethodDefinition().MakeGenericMethod(methodGenericArgs);
+        }
+
+        public static MethodInfo ConstructStaticMethodDefinition(Expression<Action> callExpr, Type[] methodGenericArgs)
+        {
+            var methodCallExpression = (MethodCallExpression)callExpr.Body;
+            MethodInfo methodInfo = methodCallExpression.Method;
+            if(!methodInfo.IsGenericMethod)
+                return methodInfo;
+            return methodInfo.GetGenericMethodDefinition().MakeGenericMethod(methodGenericArgs);
+        }
+
+        public static MethodInfo GetMethodDefinition<T>(Expression<Action<T>> callExpr)
+        {
+            var methodCallExpression = (MethodCallExpression)callExpr.Body;
+            return methodCallExpression.Method;
+        }
+
+        public static MethodInfo GetMethodDefinition<T>(Expression<Func<T, object>> callExpr)
+        {
+            Expression expression = callExpr.Body;
+            switch(expression.NodeType)
+            {
+            case ExpressionType.Convert:
+                return GetMethodDefinitionImpl(((UnaryExpression)expression).Operand);
+            default:
+                return GetMethodDefinitionImpl(expression);
+            }
+        }
+
+        public static object CallMethod<T>(T target, Expression<Action<T>> callExpr, Type[] methodGenericArgs,
+                                           object[] methodArgs)
+        {
+            MethodInfo methodInfo = ConstructMethodDefinition(callExpr, methodGenericArgs);
+            return methodInfo.Invoke(target, methodArgs);
+        }
+
+        public static object CallStaticMethod(Expression<Action> callExpr, Type[] methodGenericArgs,
+                                              object[] methodArgs)
+        {
+            MethodInfo methodInfo = ConstructStaticMethodDefinition(callExpr, methodGenericArgs);
+            return methodInfo.Invoke(null, methodArgs);
+        }
+
+        public static PropertyInfo GetProp<T, TProp>(Expression<Func<T, TProp>> readPropFunc)
+        {
+            Expression expression = readPropFunc.Body;
+            MemberInfo memberInfo = ((MemberExpression)expression).Member;
+            var propertyInfo = memberInfo as PropertyInfo;
+            if(propertyInfo == null)
+                throw new ArgumentException(string.Format("Bad expression. {0} is not a PropertyInfo", memberInfo));
+            return propertyInfo;
+        }
+
+        public static MethodInfo ConstructGenericMethodDefinitionForGenericClass<T>(Expression<Action<T>> callExpr,
+                                                                                    Type[] classGenericArgs,
+                                                                                    Type[] methodGenericArgs)
+        {
+            var methodCallExpression = (MethodCallExpression)callExpr.Body;
+            MethodInfo methodInfo = methodCallExpression.Method;
+            Type type = typeof(T);
+            Type resultReflectedType = type.IsGenericType
+                                           ? type.GetGenericTypeDefinition().MakeGenericType(classGenericArgs)
+                                           : type;
+            MethodInfo sourceMethodDefinition = methodInfo.IsGenericMethod
+                                                    ? methodInfo.GetGenericMethodDefinition()
+                                                    : methodInfo;
+            MethodBase methodBase = MethodBase.GetMethodFromHandle(
+                sourceMethodDefinition.MethodHandle, resultReflectedType.TypeHandle);
+            var constructedMethodForResultType = (MethodInfo)methodBase;
+            if(methodInfo.IsGenericMethod)
+                return constructedMethodForResultType.MakeGenericMethod(methodGenericArgs);
+            return constructedMethodForResultType;
+        }
+
+        private static ConstructorInfo ObjectConstructionFromConvert(Expression expression)
+        {
+            var unaryExpression = (UnaryExpression)expression;
+            var newExpression = (NewExpression)unaryExpression.Operand;
+            return newExpression.Constructor;
+        }
+
+        private static ConstructorInfo ObjectConstruction(Expression expression)
+        {
+            var newExpression = (NewExpression)expression;
+            return newExpression.Constructor;
+        }
+
+        private static MethodInfo GetMethodDefinitionImpl(Expression body)
+        {
+            var binaryExpression = body as BinaryExpression;
+            if(binaryExpression != null)
+                return (binaryExpression).Method;
+            var methodCallExpression = body as MethodCallExpression;
+            if(methodCallExpression != null)
+                return (methodCallExpression).Method;
+            throw new InvalidOperationException("unknown expression " + body);
+        }
+    }
+}
---
+++ GrEmit/Tests/HackHelpersTest.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Reflection;
+
+using GrEmit;
+
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class HackHelpersTest
+    {
+        [Test]
+        public void TestCall()
+        {
+            var c = new CForCallTest();
+            HackHelpers.CallMethod(c, test => test.CallInstance(-1), null, new object[] {2});
+            Assert.AreEqual(2, c.callInstance);
+            Assert.AreEqual(0, CForCallTest.callStatic);
+        }
+
+        [Test]
+        public void TestCallStatic()
+        {
+            var c = new CForCallTest();
+            HackHelpers.CallStaticMethod(() => CForCallTest.CallStatic(-1), null, new object[] {2});
+            Assert.AreEqual(0, c.callInstance);
+            Assert.AreEqual(2, CForCallTest.callStatic);
+
+            Assert.AreEqual(20,
+                            (int)
+                            HackHelpers.CallStaticMethod(() => CForCallTest.CallStaticGeneric(""), new[] {typeof(int)},
+                                                         new object[] {20}));
+        }
+
+        [Test]
+        public void TestConstructGenericMethodDefinitionForGenericClass()
+        {
+            MethodInfo constructedMethod =
+                HackHelpers.ConstructGenericMethodDefinitionForGenericClass<CGeneric<int>>(x => x.Method<long>(),
+                                                                                           new[] {typeof(string)},
+                                                                                           new[] {typeof(object)});
+            Assert.AreEqual(HackHelpers.GetMethodDefinition<CGeneric<string>>(generic => generic.Method<object>()),
+                            constructedMethod);
+
+            Assert.AreNotEqual(HackHelpers.GetMethodDefinition<CGeneric<int>>(generic => generic.Method<object>()),
+                               constructedMethod);
+            Assert.AreNotEqual(HackHelpers.GetMethodDefinition<CGeneric<int>>(generic => generic.Method<long>()),
+                               constructedMethod);
+            Assert.AreNotEqual(HackHelpers.GetMethodDefinition<CGeneric<string>>(generic => generic.Method<long>()),
+                               constructedMethod);
+        }
+
+        [Test]
+        public void TestConstructMethodDefinition()
+        {
+            Assert.AreEqual(GetMethod(typeof(ClassWithMethods), "Method", new[] {typeof(long)}),
+                            HackHelpers.ConstructMethodDefinition<ClassWithMethods>(o => o.Method<int>(),
+                                                                                    new[] {typeof(long)}));
+            Assert.AreEqual(GetMethod(typeof(ClassWithMethods), "NonGenericMeth", null),
+                            HackHelpers.ConstructMethodDefinition<ClassWithMethods>(o => o.NonGenericMeth(), null));
+            Assert.AreEqual(
+                GetMethod(typeof(ClassWithMethods), "Method2", new[] {typeof(long), typeof(object)}),
+                HackHelpers.ConstructMethodDefinition<ClassWithMethods>(o => o.Method2<int, string>(default(string)),
+                                                                        new[] {typeof(long), typeof(object)}));
+
+            Assert.AreEqual(GetMethod(typeof(CForCallTest), "CallStatic", null),
+                            HackHelpers.ConstructStaticMethodDefinition(() => CForCallTest.CallStatic(11), null));
+        }
+
+        [Test]
+        public void TestGetGenericMethodDefinitionForGenericClass2()
+        {
+            MethodInfo constructedMethod =
+                HackHelpers.ConstructGenericMethodDefinitionForGenericClass<CGeneric<int>>(x => x.Method<long>(1),
+                                                                                           new[] {typeof(string)},
+                                                                                           new[] {typeof(object)});
+            Assert.AreEqual(HackHelpers.GetMethodDefinition<CGeneric<string>>(generic => generic.Method<object>(1)),
+                            constructedMethod);
+        }
+
+        [Test]
+        public void TestGetGenericMethodDefinitionForGenericClass3()
+        {
+            MethodInfo constructedMethod =
+                HackHelpers.ConstructGenericMethodDefinitionForGenericClass<CGeneric<int>>(x => x.Method(1L, "b", 2),
+                                                                                           new[] {typeof(object)},
+                                                                                           new[] {typeof(A), typeof(B)});
+            Assert.AreEqual(
+                HackHelpers.GetMethodDefinition<CGeneric<object>>(
+                    generic => generic.Method(default(A), default(B), default(object))),
+                constructedMethod);
+        }
+
+        [Test]
+        public void TestGetGenericMethodDefinitionForGenericClassWorksForNonGenerics()
+        {
+            MethodInfo constructedMethod =
+                HackHelpers.ConstructGenericMethodDefinitionForGenericClass<ClassWithMethods>(x => x.NonGenericMeth(),
+                                                                                              null, null);
+            Assert.AreEqual(HackHelpers.GetMethodDefinition<ClassWithMethods>(generic => generic.NonGenericMeth()),
+                            constructedMethod);
+        }
+
+        [Test]
+        public void TestGetMethodDefinition()
+        {
+            Assert.AreEqual(GetMethod(typeof(Type), "op_Equality", null),
+                            HackHelpers.GetMethodDefinition<Type>(type => type == typeof(int)));
+
+            Assert.AreEqual(GetMethod(typeof(ClassWithMethods), "NonGenericMeth", null),
+                            HackHelpers.GetMethodDefinition<ClassWithMethods>(o => o.NonGenericMeth()));
+            Assert.AreEqual(GetMethod(typeof(ClassWithMethods), "op_Addition", null),
+                            HackHelpers.GetMethodDefinition<ClassWithMethods>(
+                                o => o + default(ClassWithMethods)));
+        }
+
+        [Test]
+        public void TestGetObjectConstruction()
+        {
+            Assert.AreEqual(typeof(HackHelpersTest).GetConstructor(Type.EmptyTypes),
+                            HackHelpers.GetObjectConstruction(() => new HackHelpersTest()));
+            Assert.AreEqual(typeof(string).GetConstructor(new[] {typeof(char), typeof(int)}),
+                            HackHelpers.GetObjectConstruction(() => new string('x', 1)));
+            try
+            {
+                HackHelpers.GetObjectConstruction(() => new CStruct());
+                Assert.Fail("no crash");
+            }
+            catch(NotSupportedException e)
+            {
+                Assert.AreEqual("Struct creation without arguments", e.Message);
+            }
+            Assert.AreEqual(typeof(CStruct).GetConstructor(new[] {typeof(int)}),
+                            HackHelpers.GetObjectConstruction(() => new CStruct(1)));
+
+            Assert.AreEqual(typeof(CGeneric<long>).GetConstructor(new[] {typeof(long)}),
+                            HackHelpers.GetObjectConstruction(() => new CGeneric<string>("zzz"), typeof(long)));
+        }
+
+        [Test]
+        public void TestGetValueTypeForNullableOrNull()
+        {
+            Assert.IsNull(HackHelpers.GetValueTypeForNullableOrNull(typeof(int)));
+            Assert.IsNull(HackHelpers.GetValueTypeForNullableOrNull(typeof(object)));
+            Assert.IsNull(HackHelpers.GetValueTypeForNullableOrNull(typeof(Nullable<>)));
+            Assert.IsNull(HackHelpers.GetValueTypeForNullableOrNull(typeof(int?).GetGenericArguments()[0]));
+            Assert.IsNull(HackHelpers.GetValueTypeForNullableOrNull(typeof(Generic<>)));
+            Assert.IsNull(HackHelpers.GetValueTypeForNullableOrNull(typeof(Generic<int>)));
+            Assert.AreEqual(typeof(int), HackHelpers.GetValueTypeForNullableOrNull(typeof(int?)));
+            // ReSharper disable ConvertNullableToShortForm
+            Assert.AreEqual(typeof(long), HackHelpers.GetValueTypeForNullableOrNull(typeof(Nullable<long>)));
+            // ReSharper restore ConvertNullableToShortForm
+        }
+
+        private static MethodInfo GetMethod(Type type, string name, Type[] genericArguments)
+        {
+            MethodInfo[] methods = type.GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static);
+            foreach(MethodInfo info in methods)
+            {
+                if(info.Name == name)
+                {
+                    if(genericArguments == null || genericArguments.Length == 0 && !info.IsGenericMethod)
+                        return info;
+                    if(info.IsGenericMethod)
+                        return info.MakeGenericMethod(genericArguments);
+                }
+            }
+            throw new MissingMethodException(type.Name, name);
+        }
+
+        // ReSharper disable ClassNeverInstantiated.Local
+
+        private class A
+        {
+        }
+
+        private class B
+        {
+        }
+
+        private class CGeneric<TC>
+        {
+            public CGeneric(TC c)
+            {
+            }
+
+            public CGeneric(int a)
+            {
+            }
+
+            public void Method<T>()
+            {
+            }
+
+            public void Method<T>(int x)
+            {
+            }
+
+            public void Method<T, T2>(T a, T2 b, TC c)
+            {
+            }
+        }
+
+        private class ClassWithMethods
+        {
+            // ReSharper disable MemberCanBeMadeStatic.Local
+            public void NonGenericMeth()
+            {
+            }
+
+            public void Method<T>()
+            {
+            }
+
+            public int Method2<T, TT>(TT val)
+            {
+                return 0;
+            }
+
+            public static ClassWithMethods operator +(ClassWithMethods a, ClassWithMethods b)
+            {
+                return null;
+            }
+
+            // ReSharper restore MemberCanBeMadeStatic.Local
+        }
+
+        private class Generic<T>
+        {
+        }
+
+        private class CForCallTest
+        {
+            public static TZ CallStaticGeneric<TZ>(TZ z)
+            {
+                return z;
+            }
+
+            public static void CallStatic(int a)
+            {
+                callStatic += a;
+            }
+
+            public void CallInstance(int a)
+            {
+                callInstance += a;
+            }
+
+            public static int callStatic;
+            public int callInstance;
+        }
+
+        private struct CStruct
+        {
+            public CStruct(int x)
+            {
+            }
+        }
+    }
+}
