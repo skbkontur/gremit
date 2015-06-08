bc2af48fe8d61679a10fed0fdde01e1aad0cb42d
spaceorc <spaceorc@skbkontur.ru>
2015-06-08 13:44:35 +0500

    +cm module

---
+++ build.cmd
@@ -0,0 +1,8 @@
+@echo off
+set dotNetBasePath=%windir%\Microsoft.NET\Framework
+if exist %dotNetBasePath%64 set dotNetBasePath=%dotNetBasePath%64
+for /R %dotNetBasePath% %%i in (*msbuild.exe) do set msbuild=%%i
+
+set target=GrEmit\GrEmit.sln
+
+%msbuild% /v:q /t:Rebuild /p:Configuration=Release /nodeReuse:false /maxcpucount %target%
