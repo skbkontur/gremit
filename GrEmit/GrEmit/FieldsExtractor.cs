60b70f2c80169bc90d1da3f33cfb7ad6b9a2da1f
unknown <boroka@k1506024.kontur>
2015-07-17 19:47:03 +0500

    GetSetter function

--- GrEmit/GrEmit/FieldsExtractor.cs
+++ GrEmit/GrEmit/FieldsExtractor.cs
@@ -12,7 +12,7 @@ namespace GrEmit
             var extractor = (Func<object, object>)extractors[field];
             if(extractor == null)
             {
-                lock(lockObject)
+                lock(extractorsLock)
                 {
                     extractor = (Func<object, object>)extractors[field];
                     if(extractor == null)
@@ -20,6 +20,21 @@ namespace GrEmit
                 }
             }
             return extractor;
+        }
+
+        public static Action<object, object> GetSetter(FieldInfo field)
+        {
+            var foister = (Action<object, object>)foisters[field];
+            if(foister == null)
+            {
+                lock(foistersLock)
+                {
+                    foister = (Action<object, object>)foisters[field];
+                    if(foister == null)
+                        foisters[field] = foister = BuildFoister(field);
+                }
+            }
+            return foister;
         }
 
         public static Func<T, TResult> GetExtractor<T, TResult>(FieldInfo field)
@@ -33,6 +48,19 @@ namespace GrEmit
         {
             var extractor = GetExtractor(field);
             return () => (TResult)extractor(null);
+        }
+
+        public static Action<T, TValue> GetSetter<T, TValue>(FieldInfo field)
+            where T: class
+        {
+            var foister = GetSetter(field);
+            return (inst, value) => foister(inst, value);
+        }
+
+        public static Action<TValue> GetSetter<TValue>(FieldInfo field)
+        {
+            var foister = GetSetter(field);
+            return value => foister(null, value);
         }
 
         private static Func<object, object> BuildExtractor(FieldInfo field)
@@ -57,7 +85,35 @@ namespace GrEmit
             return (Func<object, object>)dynamicMethod.CreateDelegate(typeof(Func<object, object>));
         }
 
+        private static Action<object, object> BuildFoister(FieldInfo field)
+        {
+            var methodName = "FieldFoister$";
+            if(field.IsStatic)
+                methodName += field.DeclaringType + "$";
+            methodName += field.Name + "$" + Guid.NewGuid();
+            var dynamicMethod = new DynamicMethod(methodName, typeof(void), new[] {typeof(object), typeof(object)}, typeof(FieldsExtractor), true);
+            using(var il = new GroboIL(dynamicMethod))
+            {
+                if(!field.IsStatic)
+                {
+                    il.Ldarg(0);
+                    il.Castclass(field.DeclaringType);
+                }
+                il.Ldarg(1);
+                if(field.FieldType.IsValueType)
+                    il.Unbox_Any(field.FieldType);
+                else
+                    il.Castclass(field.FieldType);
+                il.Stfld(field);
+                il.Ret();
+            }
+            return (Action<object, object>)dynamicMethod.CreateDelegate(typeof(Action<object, object>));
+        }
+
         private static readonly Hashtable extractors = new Hashtable();
-        private static readonly object lockObject = new object();
+        private static readonly object extractorsLock = new object();
+
+        private static readonly Hashtable foisters = new Hashtable();
+        private static readonly object foistersLock = new object();
     }
 }
