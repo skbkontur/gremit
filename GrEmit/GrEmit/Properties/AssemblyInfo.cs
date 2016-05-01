c8397a2bf4eae3a3f0b04bb426f36ca3d1bfd783
Igor Chevdar <homuroll@yandex.ru>
2016-05-01 14:08:15 +0500

    changed year

--- GrEmit/GrEmit/Properties/AssemblyInfo.cs
+++ GrEmit/GrEmit/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("SKB Kontur")]
 [assembly: AssemblyProduct("GrEmit")]
-[assembly: AssemblyCopyright("Copyright © 2012-2015")]
+[assembly: AssemblyCopyright("Copyright © 2012-2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
