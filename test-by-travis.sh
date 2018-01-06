c3b0c57b6b1b203959501f3d91a4179a16831567
avk <Andrew.Kostousov@gmail.com>
2018-01-06 21:33:51 +0500

    build only net45 target on mono

--- test-by-travis.sh
+++ test-by-travis.sh
@@ -10,8 +10,8 @@ then
   dotnet test --no-build --configuration Release --framework netcoreapp2.0 ./GrEmit.Tests/GrEmit.Tests.csproj
 else
   nuget install NUnit.ConsoleRunner -Version 3.7.0 -OutputDirectory testrunner
-  msbuild /t:Restore ./GrEmit.sln
-  msbuild /t:Rebuild /p:Configuration=Release ./GrEmit.sln
+  msbuild /t:Restore /p:TargetFramework=net45 ./GrEmit.sln
+  msbuild /t:Build /p:Configuration=Release /p:TargetFramework=net45 ./GrEmit.sln
   mono ./testrunner/NUnit.ConsoleRunner.3.7.0/tools/nunit3-console.exe ./GrEmit.Tests/bin/Release/net45/GrEmit.Tests.dll
 fi
 
