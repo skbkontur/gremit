18fcc9580838c02f23d0ccce893d179e3558239d
avk <Andrew.Kostousov@gmail.com>
2018-01-06 21:48:08 +0500

    use same props for restore & build

--- test-by-travis.sh
+++ test-by-travis.sh
@@ -10,7 +10,7 @@ then
   dotnet test --no-build --configuration Release --framework netcoreapp2.0 ./GrEmit.Tests/GrEmit.Tests.csproj
 else
   nuget install NUnit.ConsoleRunner -Version 3.7.0 -OutputDirectory testrunner
-  msbuild /t:Restore /p:TargetFramework=net45 ./GrEmit.sln
+  msbuild /t:Restore /p:Configuration=Release /p:TargetFramework=net45 ./GrEmit.sln
   msbuild /t:Build /p:Configuration=Release /p:TargetFramework=net45 ./GrEmit.sln
   mono ./testrunner/NUnit.ConsoleRunner.3.7.0/tools/nunit3-console.exe ./GrEmit.Tests/bin/Release/net45/GrEmit.Tests.dll
 fi
