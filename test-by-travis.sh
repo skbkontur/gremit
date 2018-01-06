612752b6bef726966f79fd79a4b0b53e05ab7a2a
avk <Andrew.Kostousov@gmail.com>
2018-01-06 19:50:49 +0500

    use msbuild instead of xbuild

--- test-by-travis.sh
+++ test-by-travis.sh
@@ -10,7 +10,7 @@ then
   dotnet test --no-build --configuration Release --framework netcoreapp2.0 ./GrEmit.Tests/GrEmit.Tests.csproj
 else
   nuget install NUnit.ConsoleRunner -Version 3.7.0 -OutputDirectory testrunner
-  xbuild /p:Configuration=Release ./GrEmit.sln
+  msbuild /p:Configuration=Release ./GrEmit.sln
   mono ./testrunner/NUnit.ConsoleRunner.3.7.0/tools/nunit3-console.exe ./GrEmit.Tests/bin/Release/GrEmit.Tests.dll
 fi
 
