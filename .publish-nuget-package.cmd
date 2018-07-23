cb4ebeba0b5c32b69c01b8b2294409a1d51ccba0
avk <Andrew.Kostousov@gmail.com>
2018-07-23 22:22:10 +0500

    switch to testing against dotnet core 2.1

--- .publish-nuget-package.cmd
+++ .publish-nuget-package.cmd
@@ -5,16 +5,16 @@ set SolutionName=GrEmit
 rem reset current directory to the location of this script
 pushd "%~dp0"
 
-if exist "./%GrEmit%/bin" (
-    rd "./%GrEmit%/bin" /Q /S || exit /b 1
+if exist "./%SolutionName%/bin" (
+    rd "./%SolutionName%/bin" /Q /S || exit /b 1
 )
 
-dotnet build --force --no-incremental --configuration Release "./%GrEmit%.sln" || exit /b 1
+dotnet build --force --no-incremental --configuration Release "./%SolutionName%.sln" || exit /b 1
 
-dotnet pack --no-build --configuration Release "./%GrEmit%.sln" || exit /b 1
+dotnet pack --no-build --configuration Release "./%SolutionName%.sln" || exit /b 1
 
-pushd "./%GrEmit%/bin/Release"
-rem dotnet nuget push *.nupkg -s https://nuget.org || exit /b 1
+pushd "./%SolutionName%/bin/Release"
+dotnet nuget push *.nupkg -s https://nuget.org || exit /b 1
 popd
 
 pause
