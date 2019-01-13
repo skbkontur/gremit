19d2ebe71118ada89cf9fc07883b68d31ee3c3a9
avk <Andrew.Kostousov@gmail.com>
2019-01-13 16:10:50 +0500

    switch tests to run on .net core 2.2

--- test-by-travis.sh
+++ test-by-travis.sh
@@ -6,8 +6,8 @@ if [ ${DOTNETCORE} -eq 1 ]
 then
   dotnet restore ./GrEmit.sln --verbosity m
   dotnet build --configuration Release --framework netstandard2.0 ./GrEmit/GrEmit.csproj
-  dotnet build --configuration Release --framework netcoreapp2.0 ./GrEmit.Tests/GrEmit.Tests.csproj
-  dotnet test --no-build --configuration Release --framework netcoreapp2.0 ./GrEmit.Tests/GrEmit.Tests.csproj
+  dotnet build --configuration Release --framework netcoreapp2.2 ./GrEmit.Tests/GrEmit.Tests.csproj
+  dotnet test --no-build --configuration Release --framework netcoreapp2.2 ./GrEmit.Tests/GrEmit.Tests.csproj
 else
   nuget install NUnit.ConsoleRunner -Version 3.7.0 -OutputDirectory testrunner
   msbuild /t:Restore /p:Configuration=Release /p:TargetFramework=net45 ./GrEmit.sln
