d64b245fabcda433c6acda96425579da7e588b40
Anton Chaplygin <anton92nd@gmail.com>
2020-08-28 11:37:45 +0500

    check codestyle before running tests & fail if codestyle violated

--- test-by-travis.sh
+++ test-by-travis.sh
@@ -7,6 +7,8 @@ then
   dotnet restore ./GrEmit.sln --verbosity m
   dotnet build --configuration Release --framework netstandard2.0 ./GrEmit/GrEmit.csproj
   dotnet build --configuration Release --framework netcoreapp3.1 ./GrEmit.Tests/GrEmit.Tests.csproj
+  dotnet jb cleanupcode GrEmit.sln --profile=CatalogueCleanup --verbosity=WARN
+  git diff --exit-code
   dotnet test --no-build --configuration Release --framework netcoreapp3.1 ./GrEmit.Tests/GrEmit.Tests.csproj
 else
   nuget install NUnit.ConsoleRunner -Version 3.9.0 -OutputDirectory testrunner
